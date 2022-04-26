57f111189f66202e1bed9649f35a61844f6f6034
tommimon <37435103+tommimon@users.noreply.github.com>
2022-04-27 00:34:30 +0200

    Update moves.md

--- src/main/java/it/polimi/ingsw/utils/Move.java
+++ src/main/java/it/polimi/ingsw/utils/Move.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.utils;
 
+import it.polimi.ingsw.server.Wizard;
+
 public abstract class Move extends Message{
     private int number = 0;
+
+    private Wizard wizard;
 
     public abstract void applyEffect(/*Model*/);
     public abstract void validate(/*Model*/) throws Exception;

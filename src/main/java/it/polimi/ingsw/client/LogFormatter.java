1f0d2bd8a05b9409789944874e1fb5dd401b550c
riccardo-negri <67798955+riccardo-negri@users.noreply.github.com>
2022-05-14 00:42:33 +0200

    Created LogFormatter for a pretty nice log

---
+++ src/main/java/it/polimi/ingsw/client/LogFormatter.java
@@ -0,0 +1,59 @@
+package it.polimi.ingsw.client;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class LogFormatter extends Formatter
+{
+    @Override
+    public String format(LogRecord record)
+    {
+        StringBuilder log = new StringBuilder();
+
+        log.append(ansi().fgBrightCyan());
+
+        log.append("[");
+        log.append(calcDate(record.getMillis()));
+        log.append("]");
+
+        log.append(" ");
+        log.append(record.getSourceClassName());
+
+        log.append("\n");
+
+        log.append(ansi().fgBrightRed());
+        log.append("[");
+        log.append(record.getLevel().getName());
+        log.append("] ");
+
+        log.append(ansi().fgDefault());
+        log.append(record.getMessage());
+
+        Object[] params = record.getParameters();
+
+        if (params != null)
+        {
+            log.append("\t");
+            for (int i = 0; i < params.length; i++)
+            {
+                log.append(params[i]);
+                if (i < params.length - 1)
+                    log.append(", ");
+            }
+        }
+
+        log.append(ansi().fgDefault());
+        log.append("\n");
+        return log.toString();
+    }
+
+    private String calcDate(long millisecond) {
+        SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date(millisecond);
+        return date_format.format(date);
+    }
+}

8fd4a7e363449b209b633ce4a0e33f6515b3a05a
tommimon <37435103+tommimon@users.noreply.github.com>
2022-06-03 01:04:27 +0200

    Both client in one executable

--- src/main/java/it/polimi/ingsw/EriantysClient.java
+++ src/main/java/it/polimi/ingsw/EriantysClient.java
@@ -1,11 +1,17 @@
 package it.polimi.ingsw;
 
 import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.client.ui.gui.GUIApplication;
 
 public class EriantysClient {
     public static void main(String[] args) {
-        Client client = new Client(false);
-        client.start();
+        if(args.length > 0 && (args[0].equals("--cli") || args[0].equals("-c"))) {
+            Client client = new Client(false);
+            client.start();
+        }
+        else {
+            GUIApplication.run(args);
+        }
     }
 
 }
--- src/main/java/it/polimi/ingsw/EriantysGUI.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw;
-
-import it.polimi.ingsw.client.ui.gui.GUIApplication;
-
-public class EriantysGUI {
-    public static void main(String[] args) {
-        GUIApplication.run(args);
-    }
-}

cc64a01c6ab671599947f6e1de26ee9f706edc64
riccardo-negri <67798955+riccardo-negri@users.noreply.github.com>
2022-06-18 23:56:05 +0200

    Working connection page

--- src/main/java/it/polimi/ingsw/client/ui/gui/controllers/ConnectionPageController.java
+++ src/main/java/it/polimi/ingsw/client/ui/gui/controllers/ConnectionPageController.java
@@ -3,10 +3,10 @@ package it.polimi.ingsw.client.ui.gui.controllers;
 import it.polimi.ingsw.client.page.AbstractConnectionPage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
-import static it.polimi.ingsw.client.page.ClientPage.LOBBY_PAGE;
+import java.util.logging.Level;
 
 public class ConnectionPageController extends AbstractController{
 
@@ -17,20 +17,33 @@ public class ConnectionPageController extends AbstractController{
     @FXML
     TextField ip;
     @FXML
-    DialogPane dialog;
+    GridPane askCreateSection;
     @FXML
     private void handleConnect(ActionEvent event) {
         AbstractConnectionPage page = (AbstractConnectionPage) client.getCurrState();
         int p = Integer.parseInt(port.getText());
-        page.connectToMatchmakingServer(ip.getText(), p, username.getText());
-        page.waitForLobbiesListOrRedirect();
-
-        if (client.getNextState() == LOBBY_PAGE) {
-            client.drawNextPage();
-        } else {
-            dialog.setVisible(true);
-            //boolean createGame = true;
-            //page.onEnd(createGame);
+        try {
+            page.connectToMatchmakingServer(ip.getText(), p, username.getText());
+            page.waitForLobbiesListOrRedirect();
         }
+        catch (Exception e) {
+            client.getLogger().log(Level.SEVERE, "Got an exception");
+        }
+
+        askCreateSection.setVisible(true);
+    }
+
+    @FXML
+    private void handleAnswerNo() {
+        AbstractConnectionPage page = (AbstractConnectionPage) client.getCurrState();
+        page.onEnd(false);
+        client.drawNextPage();
+    }
+
+    @FXML
+    private void handleAnswerYes() {
+        AbstractConnectionPage page = (AbstractConnectionPage) client.getCurrState();
+        page.onEnd(true);
+        client.drawNextPage();
     }
 }

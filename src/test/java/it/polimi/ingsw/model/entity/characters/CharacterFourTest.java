4454b55dc29191d3fcd267e86aa665eec7e70199
pietromello <pietro.mello.rella@gmail.com>
2022-06-19 18:40:19 +0200

    Character 4 test

---
+++ src/test/java/it/polimi/ingsw/model/entity/characters/CharacterFourTest.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw.model.entity.characters;
+
+import it.polimi.ingsw.model.entity.Game;
+import it.polimi.ingsw.model.entity.gameState.MoveMotherNatureActionState;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterFourTest {
+
+    private Game game;
+
+    @BeforeEach
+    void initializeGame() {
+        try{ game = Game.request(Game.deserializeGameFromFile("./src/test/java/it/polimi/ingsw/model/entity/serialized_tests/character_test/character4TestBefore.json"));
+        } catch (Exception e) { System.err.println("Can't find file in test initialization..."); }
+    }
+
+    @Test
+    void useEffect() {
+        Assertions.assertEquals("Wrong player", Assertions.assertThrows(Exception.class, () ->
+                game.getCharacter(4).characterValidator(0)
+        ).getMessage());
+        Assertions.assertDoesNotThrow(() -> {
+            ((CharacterFour) game.getCharacter(4)).useEffect(1);
+            ((MoveMotherNatureActionState) game.getGameState()).moveMotherNature(1, 7);
+            //game.serializeGame("./src/test/java/it/polimi/ingsw/model/entity/serialized_tests/character_test/character4TestAfter.json");
+            Assertions.assertEquals( new BufferedReader(new FileReader("./src/test/java/it/polimi/ingsw/model/entity/serialized_tests/character_test/character4TestAfter.json")).readLine(), game.serializeGame());
+        });
+    }
+}

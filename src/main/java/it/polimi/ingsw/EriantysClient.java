b2a6d2a0b78862a067e0ff308e033756e92e0bc5
tommimon <37435103+tommimon@users.noreply.github.com>
2022-06-19 19:33:22 +0200

    Main methods javadoc

--- src/main/java/it/polimi/ingsw/EriantysClient.java
+++ src/main/java/it/polimi/ingsw/EriantysClient.java
@@ -4,6 +4,11 @@ import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.client.ui.gui.GUIApplication;
 
 public class EriantysClient {
+
+    /**
+     * if a specific argument is present run cli else run gui application
+     * @param args program arguments set on launch
+     */
     public static void main(String[] args) {
         if(args.length > 0 && (args[0].equals("--cli") || args[0].equals("-c"))) {
             Client client = new Client();

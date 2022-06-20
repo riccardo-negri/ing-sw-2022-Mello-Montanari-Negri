a04099725badd65f63f79366f736b453348c586c
tommimon <37435103+tommimon@users.noreply.github.com>
2022-06-20 11:17:53 +0200

    Spinner for player number input

--- src/main/java/it/polimi/ingsw/client/ui/gui/controllers/CreateGamePageController.java
+++ src/main/java/it/polimi/ingsw/client/ui/gui/controllers/CreateGamePageController.java
@@ -7,19 +7,27 @@ import it.polimi.ingsw.model.enums.PlayerNumber;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
 
 public class CreateGamePageController extends AbstractController{
 
     @FXML
-    TextField playersNumber;
+    Spinner<Integer> playersNumber;
     @FXML
     CheckBox advancedRules;
 
     @FXML
+    void initialize() {
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(2,4,2);
+        playersNumber.setValueFactory(valueFactory);
+    }
+
+    @FXML
     private void handleCreate(ActionEvent event) {
         AbstractCreateGamePage page = (AbstractCreateGamePage) client.getCurrState();
-        int pn = Integer.parseInt(playersNumber.getText());
+        int pn = playersNumber.getValue();
         page.createGameAndGoToLobby(pn, advancedRules.isSelected());
         page.onEnd();
         client.drawNextPage();

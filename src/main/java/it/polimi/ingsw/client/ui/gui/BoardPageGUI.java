3ab06d61ea760f588e97b1bcf82e69713757636e
pietromello <pietro.mello.rella@gmail.com>
2022-06-24 17:00:51 +0200

    added controller for move mother nature

--- src/main/java/it/polimi/ingsw/client/ui/gui/BoardPageGUI.java
+++ src/main/java/it/polimi/ingsw/client/ui/gui/BoardPageGUI.java
@@ -7,6 +7,7 @@ import it.polimi.ingsw.model.enums.StudentColor;
 
 import java.util.Objects;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
 
 public class BoardPageGUI extends AbstractBoardPage {
 
@@ -30,36 +31,62 @@ public class BoardPageGUI extends AbstractBoardPage {
     }
 
     public void handleAssistantCard(int card) {
-        try {
-            doCardChoice(card);
-        } catch (Exception e) {
-            client.getLogger().log(Level.INFO, e.getMessage());
+        switch (model.getGameState().getGameStateName()) {
+            case "PS":
+                try {
+                    doCardChoice(card);
+                } catch (Exception e) {
+                    client.getLogger().log(Level.INFO, e.getMessage());
+                } break;
         }
+
     }
 
     public void handleDiningTable(StudentColor color) {
-        if (studentPicked == -1 || model.getWizard(client.getUsernames().indexOf(client.getUsername())).getEntranceStudents().get(studentPicked) != color)
-            System.out.println("Move of student " + model.getWizard(client.getPlayerNumber()).getEntranceStudents().get(studentPicked).toString() + " to dining table " + color.toString() + " not correct");
-        else {
-            try {
-                doStudentMovement(color, "dining-room");
-                studentPicked = -1;
-            } catch (Exception e) {
-                client.getLogger().log(Level.INFO, e.getMessage());
-            }
+        switch (model.getGameState().getGameStateName()) {
+            case "MSS":
+                if (studentPicked == -1 || model.getWizard(client.getUsernames().indexOf(client.getUsername())).getEntranceStudents().get(studentPicked) != color)
+                    logger.log(Level.INFO, "Move of student " + model.getWizard(client.getPlayerNumber()).getEntranceStudents().get(studentPicked).toString() + " to dining table " + color.toString() + " not correct");
+                else {
+                    try {
+                        doStudentMovement(color, "dining-room");
+                        studentPicked = -1;
+                    } catch (Exception e) {
+                        logger.log(Level.INFO, e.getMessage());
+                    }
+                } break;
         }
     }
 
     public void handleIsland(int islandId) {
-        if (studentPicked == -1)
-            logger.log(Level.INFO, "No student selected in entrance");
-        else {
-            try {
-                doStudentMovement(model.getWizard(client.getUsernames().indexOf(client.getUsername())).getEntranceStudents().get(studentPicked), "island-" + islandId);
-                studentPicked = -1;
-            } catch (Exception e) {
-                client.getLogger().log(Level.INFO, e.getMessage());
-            }
+        switch (model.getGameState().getGameStateName()) {
+            case "MSS":
+                if (studentPicked == -1) logger.log(Level.INFO, "No student selected in entrance");
+                else { try {
+                        doStudentMovement(model.getWizard(client.getUsernames().indexOf(client.getUsername())).getEntranceStudents().get(studentPicked), "island-" + islandId);
+                        studentPicked = -1;
+                    } catch (Exception e) {
+                        logger.log(Level.INFO, e.getMessage());
+                    }
+                } break;
+            case "MMNS":
+                try { doMotherNatureMovement(model.getIslandGroupList().indexOf(
+                        model.getIslandGroupList().stream().filter(islandGroup ->
+                                islandGroup.getIslandList().contains(model.getIsland(islandId))).toList().get(0)));
+                } catch (Exception e) {
+                    logger.log(Level.INFO, e.getMessage());
+                }
+        }
+    }
+
+    public void handleCloud(int cloudId) {
+        switch (model.getGameState().getGameStateName()) {
+            case "CCS":
+                try {
+                    doCloudChoice(cloudId);
+                } catch (Exception e) {
+                    logger.log(Level.INFO, e.getMessage());
+                } break;
         }
     }
 }

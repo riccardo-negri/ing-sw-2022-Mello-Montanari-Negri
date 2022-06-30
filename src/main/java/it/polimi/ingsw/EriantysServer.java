749e1fa46dc8ed35c82674db8b6b6083f346f12f
tommimon <37435103+tommimon@users.noreply.github.com>
2022-06-30 19:33:41 +0200

    Now debugging mode (logging) is activated only with specific command line argument

--- src/main/java/it/polimi/ingsw/EriantysServer.java
+++ src/main/java/it/polimi/ingsw/EriantysServer.java
@@ -1,16 +1,24 @@
 package it.polimi.ingsw;
 
 import it.polimi.ingsw.server.MatchmakingServer;
+import it.polimi.ingsw.utils.LogFormatter;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * entry point opf the server
  */
 public class EriantysServer {
     /**
-     * run matchmaking server
+     * run matchmaking server, if a specific argument is present activate debug mode
      * @param args program arguments set on launch (not used)
      */
     public static void main(String[] args) {
+        List<String> argsList = Arrays.asList(args);
+        if (argsList.contains("-d") || argsList.contains("--debug")) {
+            LogFormatter.setDebugActivated(true);
+        }
         new MatchmakingServer().run();
     }
 }
